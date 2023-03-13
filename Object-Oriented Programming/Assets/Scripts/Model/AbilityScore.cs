fe090aee2b7a9bbc32abd5ac28b9ad5185785676
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2023-03-13 18:16:26 +0100

    set up fields for ability scores

--- Object-Oriented Programming/Assets/Scripts/Model/AbilityScore.cs
+++ Object-Oriented Programming/Assets/Scripts/Model/AbilityScore.cs
@@ -6,9 +6,16 @@ namespace MonsterQuest
 {
     public class AbilityScore
     {
-        public int score;
-        public int modifier;
+        public int score { get; set; }
 
-
+        private int _modifier;
+        public int modifier
+        {
+            get => _modifier;
+            private set
+            {
+                _modifier = value; //Per 2 score +1 modifer, score 1 = -5. Score goes from 1-30 (-5, +10)
+            }
+        }
     }
 }
--- Object-Oriented Programming/Assets/Scripts/Model/AbilityScores.cs
+++ Object-Oriented Programming/Assets/Scripts/Model/AbilityScores.cs
@@ -6,9 +6,38 @@ namespace MonsterQuest
 {
     public class AbilityScores
     {
-        public AbilityScores()
-        {
+        [field: SerializeField]
+        private AbilityScore _strenght;
+        public AbilityScore strenght => _strenght;
 
+        [field: SerializeField]
+        private AbilityScore _dexterity;
+        public AbilityScore dexterity => _dexterity;
+
+        [field: SerializeField]
+        private AbilityScore _constitusion;
+        public AbilityScore constitusion => _constitusion;
+
+        [field: SerializeField]
+        private AbilityScore _wisdom;
+        public AbilityScore wisdom => _wisdom;
+
+        [field: SerializeField]
+        private AbilityScore _intelligence;
+        public AbilityScore intelligence => _intelligence;
+
+        [field: SerializeField]
+        private AbilityScore _charisma;
+        public AbilityScore charisma => _charisma;
+
+        public AbilityScores(AbilityScore strenght, AbilityScore dexterity, AbilityScore constitusion, AbilityScore wisdom, AbilityScore intelligence, AbilityScore charisma)
+        {
+            _strenght = strenght;
+            _dexterity = dexterity;
+            _constitusion = constitusion;
+            _wisdom = wisdom;
+            _intelligence = intelligence;
+            _charisma = charisma;
         }
     }
 }
