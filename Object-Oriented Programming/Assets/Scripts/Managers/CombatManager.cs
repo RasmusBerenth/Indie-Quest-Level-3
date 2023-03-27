eafc8dc4544457f1db6084d4877dd423c93e557e
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2023-03-27 15:59:04 +0200

    minor changes

--- Object-Oriented Programming/Assets/Scripts/Managers/CombatManager.cs
+++ Object-Oriented Programming/Assets/Scripts/Managers/CombatManager.cs
@@ -17,7 +17,7 @@ namespace MonsterQuest
 
             initativeList.Add(monster);
 
-            Console.WriteLine($"{monster.displayName.ToUpperCase()} with {monster.hitPoints}HP appears");
+            Console.WriteLine($"A {monster} with {monster.hitPoints}HP appears");
 
             initativeList.Shuffle();
             bool combatEnded = false;
