5e7b56f1382c81dc8bbc05d34850386339655558
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2025-01-14 10:30:39 +0100

    nothing

--- Object-Oriented Programming/Assets/Scripts/Model/Combat.cs
+++ Object-Oriented Programming/Assets/Scripts/Model/Combat.cs
@@ -1,17 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace MonsterQuest
 {
+    [Serializable]
     public class Combat
     {
+
         public Monster monster { get; private set; }
 
         public Combat(Monster monster)
         {
             this.monster = monster;
         }
-
     }
+
 }
+
